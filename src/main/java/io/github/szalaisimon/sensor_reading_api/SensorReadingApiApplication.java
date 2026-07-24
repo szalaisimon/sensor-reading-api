c988559d4e91a5a53710a4391a3a2af9f660a5fe
SzSimon <mpcint@inf.elte.hu>
2026-07-24 13:05:24 +0200

    Initial commit

---
+++ src/main/java/io/github/szalaisimon/sensor_reading_api/SensorReadingApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.szalaisimon.sensor_reading_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorReadingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorReadingApiApplication.class, args);
+	}
+
+}
