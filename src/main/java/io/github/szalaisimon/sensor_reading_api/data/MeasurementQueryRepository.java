125f5eb3605cfc3410901aeba52dc35f66f19ad6
Szalai Simon <szalai.simon@gmail.com>
2026-07-27 15:26:13 +0200

    replace postconstruct

--- src/main/java/io/github/szalaisimon/sensor_reading_api/data/MeasurementQueryRepository.java
+++ src/main/java/io/github/szalaisimon/sensor_reading_api/data/MeasurementQueryRepository.java
@@ -3,9 +3,7 @@ package io.github.szalaisimon.sensor_reading_api.data;
 import io.github.szalaisimon.sensor_reading_api.domain.model.DailyStatistics;
 import io.github.szalaisimon.sensor_reading_api.domain.model.Measurement;
 import jakarta.annotation.Nullable;
-import jakarta.annotation.PostConstruct;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
@@ -13,39 +11,43 @@ import java.time.ZoneOffset;
 import java.util.*;
 import java.util.stream.Stream;
 
-@RequiredArgsConstructor
 @Slf4j
 public class MeasurementQueryRepository {
 
-    private final @NonNull MeasurementSource measurementSource;
-    private final @NonNull Map<Long, NavigableMap<LocalDate, DailyStatistics>> measurements = new HashMap<>();
+    private final @NonNull Map<Long, NavigableMap<LocalDate, DailyStatistics>> measurements;
 
-    @PostConstruct
-    private void init() {
+    public MeasurementQueryRepository(final @NonNull MeasurementSource measurementSource) {
+        this.measurements = load(measurementSource);
+    }
+
+    private @NonNull Map<Long, NavigableMap<LocalDate, DailyStatistics>> load(final @NonNull MeasurementSource measurementSource) {
+        final @NonNull Map<Long, NavigableMap<LocalDate, DailyStatistics>> result = new HashMap<>();
+
         try (final @NonNull Stream<Measurement> measurementStream = measurementSource.getMeasurements()) {
             measurementStream.forEach(measurement -> {
-                final @Nullable NavigableMap<LocalDate, DailyStatistics> dailyStatisticsMap = measurements.get(measurement.deviceId());
+                final @Nullable NavigableMap<LocalDate, DailyStatistics> dailyStatisticsMap = result.get(measurement.deviceId());
                 final @NonNull LocalDate currentLocalDate = LocalDate.ofInstant(measurement.measureTime(), ZoneOffset.UTC);
 
                 if (dailyStatisticsMap == null) {
                     // new device
                     final @NonNull NavigableMap<LocalDate, DailyStatistics> newDailyStatistics = new TreeMap<>();
                     newDailyStatistics.put(currentLocalDate, DailyStatistics.from(measurement));
-                    measurements.put(measurement.deviceId(), newDailyStatistics);
+                    result.put(measurement.deviceId(), newDailyStatistics);
                 } else {
                     // device already set
-                    final @NonNull LocalDate measuredLocalDate = currentLocalDate;
-                    final @Nullable DailyStatistics dailyStatistics = dailyStatisticsMap.get(measuredLocalDate);
+                    final @Nullable DailyStatistics dailyStatistics = dailyStatisticsMap.get(currentLocalDate);
 
                     if (dailyStatistics == null) {
                         // new LocalDate
-                        dailyStatisticsMap.put(measuredLocalDate, DailyStatistics.from(measurement));
+                        dailyStatisticsMap.put(currentLocalDate, DailyStatistics.from(measurement));
                     } else {
                         dailyStatistics.update(measurement);
                     }
                 }
             });
         }
+
+        return result;
     }
 
     public @NonNull Set<Long> getDeviceIds() {
