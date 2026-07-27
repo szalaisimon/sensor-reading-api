684be72d6dd6c73f51d064a671bfd1bbf58b7c04
Szalai Simon <szalai.simon@gmail.com>
2026-07-27 14:42:20 +0200

    add measurement query service

---
+++ src/main/java/io/github/szalaisimon/sensor_reading_api/domain/service/MeasurementQueryService.java
@@ -0,0 +1,75 @@
+package io.github.szalaisimon.sensor_reading_api.domain.service;
+
+import io.github.szalaisimon.sensor_reading_api.data.MeasurementQueryRepository;
+import io.github.szalaisimon.sensor_reading_api.domain.model.*;
+import jakarta.annotation.Nullable;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Slf4j
+public class MeasurementQueryService {
+
+    private final @NonNull MeasurementQueryRepository measurementQueryRepository;
+
+    public @NonNull MeasurementQueryResult query(final @NonNull MeasurementQuery request) {
+        final @NonNull MeasurementQuery query = normalize(request);
+        final @NonNull List<MeasurementQueryResult.DeviceStatistics> deviceStatistics = new ArrayList<>();
+
+        query.deviceIds().forEach(deviceId -> {
+            final @NonNull Map<LocalDate, DailyStatistics> dailyStatistics = measurementQueryRepository.getDailyStatistics(deviceId, query.from(), query.to());
+
+            //aggregate
+            final @NonNull MetricStatistics aggregatedTemperature = MetricStatistics.empty();
+            final @NonNull MetricStatistics aggregatedHumidity = MetricStatistics.empty();
+
+            dailyStatistics.forEach((key, value) -> {
+                aggregatedTemperature.update(value.temperature());
+                aggregatedHumidity.update(value.humidity());
+            });
+
+            deviceStatistics.add(new MeasurementQueryResult.DeviceStatistics(
+                    deviceId,
+                    toStatisticMap(query.metrics(), query.statistic(), Metric.TEMPERATURE, aggregatedTemperature),
+                    toStatisticMap(query.metrics(), query.statistic(), Metric.HUMIDITY, aggregatedHumidity)
+            ));
+        });
+        return new MeasurementQueryResult(query, deviceStatistics);
+    }
+
+    private @NonNull MeasurementQuery normalize(final @NonNull MeasurementQuery request) {
+        if (request.from() != null && request.to() != null && request.from().isAfter(request.to())) {
+            throw new IllegalArgumentException("'from' (" + request.from() + ") must not be after 'to' (" + request.to() + ")");
+        }
+
+        return new MeasurementQuery(
+                request.deviceIds() == null || request.deviceIds().isEmpty()
+                        ? measurementQueryRepository.getDeviceIds()
+                        : request.deviceIds(),
+                request.from(),
+                request.to(),
+                request.metrics(),
+                request.statistic() == null ? Statistic.AVERAGE : request.statistic()
+        );
+    }
+
+    private @Nullable Map<Statistic, Double> toStatisticMap(
+            final @NonNull Set<Metric> metrics,
+            final @NonNull Statistic statistic,
+            final @NonNull Metric metric,
+            final @NonNull MetricStatistics statistics
+    ) {
+        if (!metrics.contains(metric) || statistics.isEmpty()) {
+            return null;
+        }
+
+        return Map.of(statistic, statistics.get(statistic));
+    }
+}
